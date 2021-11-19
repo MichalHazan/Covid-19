6f9fb6913f9d2fe3908f1d17ff5bb9f1b68c69f2
Michal Hazan <michalhazansnt@gmail.com>
2021-11-19 14:54:04 +0200

    WithSort&Moments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
 let countries = []
 const inp = document.querySelector('input')
+let order = "desc"
 
 const updateGlobalData = ({ TotalDeaths, TotalConfirmed, NewDeaths, NewConfirmed }) => {
     document.querySelector('#gct').textContent = TotalConfirmed.toLocaleString('en-IL')
@@ -13,15 +15,26 @@ const displayCountriesTable = () => {
     table.innerHTML = `
     <thead>
         <tr>
-         <th>Country</th>
-         <th>Daily Cases</th>
-         <th>Total Cases</th>
-         <th>Daily Deaths</th>
-         <th>Total Deaths</th>
+           <th data-column="Country" data-order=${order}>Country <i class="bi bi-arrow-down-up"></i></th>
+           <th data-column="NewConfirmed" data-order=${order}>Daily Cases <i class="bi bi-arrow-down-up"></i> </th>
+           <th data-column="TotalConfirmed" data-order=${order}>Total Cases <i class="bi bi-arrow-down-up"></i> </th>
+           <th data-column="NewDeaths" data-order=${order}>Daily Deaths <i class="bi bi-arrow-down-up"></i> </th>
+           <th data-column="TotalDeaths" data-order=${order}>Total Deaths <i class="bi bi-arrow-down-up"></i> </th>
         </tr>
     </thead>
+
     `
     const filterCountries = countries.filter(country => country.Country.toLowerCase().includes(inp.value.toLowerCase()))
+
+    if (!filterCountries.length) {
+        table.innerHTML = `
+        <thead>
+        <tr>
+        <th>couldn't find ${inp.value}</th>
+        </tr>
+        </thead>
+        `
+    }
 
     for (const country of filterCountries) {
 
@@ -35,9 +48,32 @@ const displayCountriesTable = () => {
         </tr>
         `
     }
+    SortBy()
+}
+//----Sort-----------------------
+function SortBy() {
 
+    $('th').on("click", function () {
+        order = $(this).data('order')
+        let column = $(this).data('column')
+        console.log("you clicked me🤗", order, column);
+
+        if (order == 'desc') {
+            order = "asc"
+            $(this).data('order', "asc")
+            countries = countries.sort((a, b) => a[column] > b[column] ? 1 : -1)
+        } else {
+            order = "desc"
+            $(this).data('order', "desc")
+            countries = countries.sort((a, b) => a[column] < b[column] ? 1 : -1)
+
+        }
+        displayCountriesTable()
+
+    })
 
 }
+let p = document.querySelector(".moment")
 const getData = async () => {
     try {
         const res = await fetch("https://api.covid19api.com/summary")
@@ -46,6 +82,9 @@ const getData = async () => {
         updateGlobalData(data.Global)
         displayCountriesTable()
 
+        let m = moment(countries[0].Date)
+        console.log(m.startOf('hour').fromNow())
+        p.textContent = "last update was " + m.startOf('hour').fromNow()
     } catch (error) {
         console.log(error)
     }
@@ -54,3 +93,4 @@ const getData = async () => {
 getData()
 
 inp.addEventListener("keyup", displayCountriesTable)
+
